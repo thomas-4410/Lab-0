73a19458f1883fd0f41a31e53bee7b4d1e4a2fe3
MAbuShattal23 <abushattal@ieee.org>
2024-02-10 21:08:12 -0500

    Adding more Javascript contents.

--- myscripts.js
+++ myscripts.js
@@ -1,3 +1,18 @@
+console.log("test");
+for(let i = 0; i< 5; i++){
+    console.log(i+"\n");
+}
+
+function Sum(){
+    let a = [1, 2, 3];
+    let total = 0;
+    for(let i = 0; i< 5; i++){
+        total += i;
+    }
+    return total;
+}
+console.log("Array sum: "+Sum());
+
 var emailTextBox = document.getElementById("email");
 var feedbackTextBox = document.getElementById("feedback");
 var button2 = document.getElementById("button2");
@@ -6,12 +21,13 @@ var submitButton =document.getElementById("mybutton");
 button2.onclick = function printText(){
 myText.value = emailTextBox.value;
 myText.value+="\n"+  feedbackTextBox.value;
+myText.value += "\nArray sum: "+ Sum();
 }
 
 submitButton.onclick = function printAlert(){
     alert(emailTextBox.value+ "\n"+  feedbackTextBox.value);
-}
 
+}
 
 
 
